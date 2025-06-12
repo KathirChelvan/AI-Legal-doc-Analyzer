aed84af54ce3be215f9973ff2266f79295647185
Kathir Chelvan I <160463505+KathirChelvan@users.noreply.github.com>
2025-06-12 20:30:25 +0530

    main.py

--- legal-analyzer/backend/main.py
+++ legal-analyzer/backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Legal Document Analyzer API")
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://ai-legal-doc-analyzer.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
